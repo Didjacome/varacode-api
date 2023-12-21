f6d552c1eb6b3f3431a4a377accdf70c54bd4bad
Diogo De Santana Jacome <dsjacome@stefanini.com>
2023-12-21 16:37:17 -0300

    add apiRequest pkg apiRequest contains an ApiRequest function that performs the operation on the veracode apis waiting for a response to be referenced when the function returns and is saved in a response.json file

---
+++ pkg/apiRequest/apiRequest.go
@@ -0,0 +1,58 @@
+package apirequest
+
+import(
+	"net/http"
+	"net/url"
+	"github.com/Didjacome/hmac-veracode/hmac"
+	"io"
+	"os"
+)
+
+func ApiRequest(apiKeyID, apiKeySecret, apiUrl, httpMethod string) string {
+	parsedUrl, err := url.Parse(apiUrl)
+	if err != nil {
+		panic(err)
+	}
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest(httpMethod, parsedUrl.String(), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	authorizationHeader, err := hmac.CalculateAuthorizationHeader(parsedUrl, httpMethod, apiKeyID, apiKeySecret)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Add("Authorization", authorizationHeader)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		panic("Expected status 200. Status was: " + resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+
+	file, err := os.Create("response.json")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	_, err = io.Copy(file, bytes.NewReader(body))
+	if err != nil {
+		panic(err)
+	}
+
+	return string(body[:])
+}
